af8306c26949cc62bb8b94fa5379167db50e1632
Eduardo Dorneles <eduardo.dorneles.fs@gmail.com>
2022-04-04 22:56:32 -0300

    componentização de alguns elementos do projeto. Migração dos jsx do content e da sidebar para componentes especializados.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,19 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "../definitions/types"
+import { MovieCard } from "./MovieCard"
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+
+export function Content( props: ContentProps ) {
+  return (
+  <main>
+    <div className="movies-list">
+      {props.movies.map(movie => (
+        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
+import { GenreName } from "../definitions/types";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: GenreName;
+    title: string;
+  }>;
+  selectedGenreId: number;
+  handleClickButton( genreId: number ): void;
+}
+
+export function SideBar( props: SideBarProps ) {
   // Complete aqui
+
+  return (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+    {props.genres.map(genre => (
+      <Button
+        key={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => props.handleClickButton(genre.id)}
+        selected={props.selectedGenreId === genre.id}
+      />
+    ))}
+    </div>
+  </nav>);
 }
